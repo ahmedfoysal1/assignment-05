c3be073faca676d050ccb591ef8688171322c4f1
Foysal <ahmedmdfoysal123@gmail.com>
2024-09-24 22:42:27 +0600

    fixed respoinsive issue

--- js/features.js
+++ js/features.js
@@ -3,10 +3,12 @@ document.getElementById('donation-btn').addEventListener('click', function(){
     // document.getElementById('donation-btn').style.backgroundColor = 'rgb(180, 244, 97)';
     showColor('donation-btn');
     showBtn('main-section');
+    document.getElementById('footer-id').classList.remove('hidden');
 
 })
 
 document.getElementById('donation-history').addEventListener('click',function(){
-    showColor('donation-history')
-    showBtn('history-section')
+    showColor('donation-history');
+    showBtn('history-section');
+    document.getElementById('footer-id').classList.add('hidden');
 })
